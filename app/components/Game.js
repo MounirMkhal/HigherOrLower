82b979ef684715c7bb33708ac4abbc69b27eff74
MounirMkhal <156144218+MounirMkhal@users.noreply.github.com>
2024-12-11 12:12:55 -0800

    Update Game.js

--- app/components/Game.js
+++ app/components/Game.js
@@ -70,7 +70,7 @@ export default function Game({ onGameOver }) {
           >
             <img src="/images/x.png" alt="Twitter" className="w-8 h-8" />
           </a>
-          <a href="https://pump.fun/coin/" target="_blank" rel="noopener noreferrer">
+          <a href="https://pump.fun/coin/FYGa1McDMTEteDBZJGwJt6h3rNYHnwYbud3ETjJ6pump" target="_blank" rel="noopener noreferrer">
             <img
               src="/images/pumpfun.png"
               alt="Chart Line"
@@ -132,7 +132,7 @@ export default function Game({ onGameOver }) {
           </div>
         </div>
         <p className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white font-bold text-2xl">
-          CA: Coming Soon...
+          CA: FYGa1McDMTEteDBZJGwJt6h3rNYHnwYbud3ETjJ6pump
         </p>
       </div>
     );
